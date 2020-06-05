1e936419729ac2afb4baae7e01bd926328c5588a
Dmitry Sychev <axiumone@gmail.com>
2020-06-05 21:24:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
-# Write your methods here
+def loop_message_five_times(string)
+  i = 0
+
+  while i < array.length
+    puts string
+    i++
+  end
+end
+
+def loop_message_n_times()
+
+end
+
+def output_array()
+
+end
+
+def return_string_array()
+
+end
